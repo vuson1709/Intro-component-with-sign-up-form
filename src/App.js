044fe003aec3b95141954d4701211eb1f7bb811d
vuson1709 <thaison170996@gmail.com>
2024-01-04 20:35:39 -0800

    edit label and input for pseudo class in error state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 /*
          
-
-- View the optimal layout for the site depending on their device's screen size
-- See hover states for all interactive elements on the page
 - Receive an error message when the `form` is submitted if:
   - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
   - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*
 
 */
 
+import { useState } from "react";
 import IconError from "./images/icon-error.svg";
 
 export default function App() {
+  const [isInputEmpty, setIsInputEmpty] = useState(false);
+
   return (
     <div className="container">
       <div className="text-content">
@@ -27,10 +27,18 @@ export default function App() {
           <strong>Try it free 7 days</strong> then $20/mo. thereafter
         </p>
         <form className="trial-form">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="text" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          <label className="required">
+            <input type="text" placeholder="First Name" />
+          </label>
+          <label className="required">
+            <input type="text" placeholder="Last Name" />
+          </label>
+          <label className="required">
+            <input type="text" placeholder="Email Address" />
+          </label>
+          <label className="required">
+            <input type="password" placeholder="Password" />
+          </label>
           <button>Claim your free trial</button>
           <p className="trial-tos">
             By clicking the button, you are agreeing to our{" "}
